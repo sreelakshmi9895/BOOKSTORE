266687b116f920c14d42684b92819f4c917fb8be
sreelakshmi9895 <sreelakshmivenu9895@gmail.com>
2025-11-28 20:34:06 +0530

    design-profile

--- src/users/components/BookStatus.jsx
+++ src/users/components/BookStatus.jsx
@@ -20,10 +20,13 @@ function BookStatus() {
   <img  width={'90px'} height={'90px'} src="https://w7.pngwing.com/pngs/568/351/png-transparent-computer-icons-sold-out-miscellaneous-text-logo.png" alt="" />
    </div>
     </div>
-   </div>
-   <img w-50 src="https://speakingtigerbooks.com/wp-content/uploads/2024/10/Everyday-Reading_Final.jpg" alt="" />
-   <button className='p-2 bg-red-600 txt-white'>DELETE</button>
+   
+  <div className='px-4 mt-4 md:mt-0'>
+       <img className='w-50' src="https://speakingtigerbooks.com/wp-content/uploads/2024/10/Everyday-Reading_Final.jpg" alt="" />
+       <div className='flex justify-end'><button className='p-2 bg-red-600 txt-white mt-5'>DELETE</button></div>
+  </div>
       </div>
+    </div>
     </div>
   )
 }
--- src/users/components/Purchase.jsx
+++ src/users/components/Purchase.jsx
@@ -2,8 +2,28 @@ import React from 'react'
 
 function Purchase() {
   return (
+     <div className='p-10 my-20 mx-5 shadow rounded'>
+      {/* book div duplicate  */}
+      <div className='bg-gray-200 p-5 rounded'>
+   <div className='md:grid grid-cols-1[3fr_1fr]'>
     <div>
-      Purchase
+   <h2 className='text-2xl'>Title</h2>
+   <h3 className='text-xl'>Author</h3>
+   <h4  className='text-lg text-red-500'>$ 400</h4>
+   <p className='text-justify'>Abstract</p>
+   <div className='flex mt-5'>
+    {/* purchase */}
+  <img width={'90px'} height={'90px'}  src="https://static.vecteezy.com/system/resources/previews/023/629/698/non_2x/web-button-icon-purchase-button-free-png.png" alt="purchase" />
+  
+   </div>
+    </div>
+   
+  <div className='px-4 mt-4 md:mt-0'>
+       <img className='w-50' src="https://speakingtigerbooks.com/wp-content/uploads/2024/10/Everyday-Reading_Final.jpg" alt="" />
+       
+  </div>
+      </div>
+    </div>
     </div>
   )
 }
